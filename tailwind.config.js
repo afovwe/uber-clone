47c33fb90d00b7913ee198adf1340967b0c235f0
Omofovwe Akpadaka <afovwe@gmail.com>
2024-08-30 12:25:38 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [
+  "./app/**/*.{js,jsx,ts,tsx}",
+  "./components/**/*.{js,jsx,ts,tsx}",
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
