34f7c1cd6464d2e93cd161448acc44632a22c0a2
Omofovwe Akpadaka <afovwe@gmail.com>
2024-09-01 16:37:42 +0100

    complete swiper

---
+++ app/(auth)/sign-in.tsx
@@ -0,0 +1,13 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const SingIn = () => {
+
+    return (
+        <SafeAreaView >
+            <Text>Sing In</Text>
+        </SafeAreaView>
+    );
+};
+
+export default SingIn;
