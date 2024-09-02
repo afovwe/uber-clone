b1f6d546178f431ce07748b2718ed887de587b95
Omofovwe Akpadaka <afovwe@gmail.com>
2024-09-02 22:10:47 +0100

    complete sign up functionality

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,10 @@ import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 
+import { ClerkProvider, ClerkLoaded } from '@clerk/clerk-expo'
+import { Slot } from 'expo-router'
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
 
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -20,6 +24,13 @@ export default function RootLayout() {
     "Jakarta-SemiBold": require("../assets/fonts/PlusJakartaSans-SemiBold.ttf"),
   });
 
+  if (!publishableKey) {
+  throw new Error(
+    "Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env",
+  );
+}
+
+
   useEffect(() => {
     if (loaded) {
       SplashScreen.hideAsync();
@@ -31,12 +42,16 @@ export default function RootLayout() {
   }
 
   return (
+     <ClerkProvider publishableKey={publishableKey}>
+      <ClerkLoaded>       
        <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="(root)" options={{ headerShown: false }} />
         <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
-      </Stack>
+      </Stack>    
+      </ClerkLoaded>
+    </ClerkProvider>
 
   );
 }
